6b1d708a3b3bcfab96ab93bbcf525b35ac19a323
shashkov <shashkov.email@gmail.com>
2019-02-03 20:48:29 +0200

    user registration auth, auth and registration blade, employe table migration, department table migration

---
+++ database/migrations/2019_02_03_144745_create_employe_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employe', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('position');
+            $table->integer('department_id');
+            $table->double('salary',15,2);
+            $table->bigInteger('boss_id')->nullable(); // if no boss, then null
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employe');
+    }
+}
