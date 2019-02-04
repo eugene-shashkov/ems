e73ed90174bc7f0d265b4ddcfa4bdd43d32344e2
shashkov <shashkov.email@gmail.com>
2019-02-04 13:23:38 +0200

    new features

---
+++ app/Employees.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employees extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'employees';
+}
